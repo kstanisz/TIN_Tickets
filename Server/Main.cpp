fb0833340d5ba50032d56a2df7599cc55363fa8a
Kacper Staniszewski <staniszewski.kacper@gmail.com>
2017-01-08 17:26:17 +0100

    Server select()

--- Server/Main.cpp
+++ Server/Main.cpp
@@ -1,41 +1,128 @@
 #include<iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
+#include <fstream>
+
+#include <set>
+#include <map>
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
 
 #include"TicketDao.h"
 
+
+void serverLoop(int udpSocket, int tcpSocket){
+	
+	fd_set fds;
+	
+	std::set <int> clientFDs;
+	
+	while(1){
+		// zeruj zbiór deskryptorach
+		FD_ZERO(&fds);
+		// dodanie deskryptorów
+		FD_SET(udpSocket, &fds);
+		FD_SET(tcpSocket, &fds);
+		
+		int max = std::max(udpSocket,tcpSocket);
+		for (auto fd : clientFDs) {
+			max = std::max(fd, max);
+			FD_SET (fd, &fds);
+		}
+		
+		int nfds = select(max+1, &fds, nullptr, nullptr, nullptr);
+		if(nfds == -1){
+			perror("select() error");
+		}
+			
+		if(nfds == 0){
+			// handle timeout
+		}
+		
+		if(FD_ISSET(tcpSocket,&fds)){
+			struct sockaddr address;
+			socklen_t addressSize = sizeof(address);
+			int newFd = accept(tcpSocket,&address,&addressSize);
+			if(newFd == -1){
+				perror("Tcp accept()");
+				continue;
+			}
+			
+			clientFDs.insert(newFd);
+		}
+		
+		if(FD_ISSET(udpSocket,&fds)){
+			char buf[4096];
+			int numBytes = recv(udpSocket,buf,sizeof(buf),0);
+			
+			std::cout<<"UDP_RECEIVE"<<std::endl;
+			std::cout<<buf<<std::endl;
+		}
+		
+		for(auto fd : clientFDs){
+			if(!FD_ISSET(fd,&fds)){
+				continue;
+			}
+			
+			char buf[4096];
+			int numBytes = recv(fd,buf,sizeof(buf),0);
+			std::cout<<"TCP_RECEIVE"<<std::endl;
+			std::cout<<buf<<std::endl;
+		}
+	}
+}
+
+
 int main(){
 	
-	TicketDao* ticketDao = new TicketDao();
+	// Utworzenie socketów i ich adresów oraz bind.
 	
-	bool connection = ticketDao->connect();
-	if(!connection)
-	{
-		std::cout<<"Nie udało się nawiązać połączenia"<<std::endl;
-		return 0;
+	const int udpSocket = socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP);
+	if(udpSocket == -1){
+		perror("Error creating udp socket.");
+		exit(1);
 	}
 	
-	bool access = ticketDao->checkAccess("127.0.0.0","password1","SERVICE1");
-	if(access)
-	{
-		std::cout<<"Użytkownik ma dostęp do serwisu"<<std::endl;
-	}
-	else
-	{
-		std::cout<<"Użytkownik nie ma dostępu do serwisu"<<std::endl;
+	struct sockaddr_in udp_address;
+	udp_address.sin_family = AF_INET;
+	udp_address.sin_addr.s_addr = INADDR_ANY;
+	udp_address.sin_port = htons(9001);
+	
+	if(bind(udpSocket, (const struct sockaddr *) & udp_address, sizeof(udp_address)) == -1){
+		perror("Error binding udp socket.");
+		exit(1);
 	}
 	
-	ticketDao->releaseTicket("127.0.0.0","password1","SERVICE1");
-	
-	bool isTicket = ticketDao->checkTicket("127.0.0.0","password1","SERVICE1");
-	if(isTicket)
-	{
-		std::cout<<"Bilet aktualny"<<std::endl;
+	const int tcpSocket = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+	if(tcpSocket == -1){
+		perror("Error creating tcp socket.");
+		exit(1);
 	}
-	else
-	{
-		std::cout<<"Brak biletu"<<std::endl;
+			
+	struct sockaddr_in tcp_address;
+	tcp_address.sin_family = AF_INET;
+	tcp_address.sin_addr.s_addr = INADDR_ANY;
+	tcp_address.sin_port = htons(9002);
+	
+		
+	if(bind(tcpSocket, (const struct sockaddr *) & tcp_address, sizeof(tcp_address)) == -1){
+		perror("Error binding tcp socket.");
+		exit(1);
 	}
 	
-	ticketDao->disconnect();
+	// Ustawienie socketa TCP na nasłuchiwanie
+	
+	listen(tcpSocket,999);
+	
+	serverLoop(udpSocket,tcpSocket);
 	
 	return 0;
 }
--- readme.txt
+++ readme.txt
@@ -1,3 +1,5 @@
 Instalacja connectora MySQL:
 
 sudo apt-get install libmysqlclient-dev
+
+UDP - brak nagłówka, wszystko w buforze - http://stackoverflow.com/questions/9304447/how-can-i-access-and-modify-my-udp-header
